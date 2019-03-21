96b4618ddfb7d86b75a0b05510afcaecb221fa7e
Christopher <38436883+G4mingEdition@users.noreply.github.com>
2019-03-21 12:36:22 +0100

    Upload

---
+++ src/be/christopher/main/MainFrame.java
@@ -0,0 +1,14 @@
+package be.christopher.main;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	
+	/* CONSTRUCTOR */
+	public MainFrame(Model model) {
+		super("Drawing - " + model.getName() + " : " + model.getWidth() + "x" + model.getHeight());
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+}
